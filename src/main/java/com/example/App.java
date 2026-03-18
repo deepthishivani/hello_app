d794a26c3e9c6c84ab89d9f600650ca67988c7db
deepthishivani <deepthishivani@gmail.com>
2026-03-18 10:35:19 +0530

    Initial Maven project setup

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else if (args.length == 1) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello " + args[0]);
+        }
+    }
+}
