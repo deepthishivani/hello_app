e2e81d3fa1e18862bb588483ff858d94a11d6dc8
deepthishivani <deepthishivani@gmail.com>
2026-03-18 10:37:01 +0530

    UC3: Display Hello with argument or default

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,12 +2,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello World");
-        } else if (args.length == 1) {
+        if (args.length > 0) {
             System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello " + args[0]);
+            System.out.println("Hello World");
         }
     }
 }
