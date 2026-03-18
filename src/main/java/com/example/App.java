461ea92dbb32d547cf01c48ef785f0c7c3bc19dc
deepthishivani <deepthishivani@gmail.com>
2026-03-18 10:40:24 +0530

    UC1: Display Hello World

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,8 +4,6 @@ public class App {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello World");
-        } else if (args.length == 1) {
-            System.out.println("Hello " + args[0]);
         } else {
             System.out.println("Hello " + args[0]);
         }
