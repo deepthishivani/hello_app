bd990b58b61165cc6a2661bbf5541f7a50f68876
deepthishivani <deepthishivani@gmail.com>
2026-04-10 08:28:35 +0530

    UC7: Using String.join

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,9 @@ public class App {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello World");
-        } else if (args.length == 1) {
-            System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello " + args[0]);
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names);
         }
     }
 }
