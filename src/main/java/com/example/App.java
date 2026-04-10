253d568cf8299803e8ef552be03f1c04b05bdfdb
deepthishivani <deepthishivani@gmail.com>
2026-04-10 08:22:52 +0530

    UC5: Enhanced for loop version

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,10 @@ public class App {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello World");
-        } else if (args.length == 1) {
-            System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello " + args[0]);
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
     }
 }
