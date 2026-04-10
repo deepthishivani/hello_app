8c4da2326a8f60f82104ddbfbfdb6a75ed7a6c2b
deepthishivani <deepthishivani@gmail.com>
2026-04-10 08:20:00 +0530

    UC4: Display Hello with multiple names

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,11 +3,10 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello World");
-        } else if (args.length == 1) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello " + args[0]);
-        }
+    System.out.println("Hello World");}
+       else {
+    for (int i = 0; i < args.length; i++) {
+        System.out.println("Hello " + args[i]);
     }
 }
+    }}
